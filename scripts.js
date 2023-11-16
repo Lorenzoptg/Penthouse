5c62b324136ccc64469df2486ec25f4d88231621
Lorenzo <92269232+Lorenzoptg@users.noreply.github.com>
2023-11-16 14:56:48 +0100

    ok

--- scripts.js
+++ scripts.js
@@ -1,7 +1,37 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+var path = document.querySelector('#star-path');
+
+
+var pathLength = path.getTotalLength();
+
+
+path.style.strokeDasharray = pathLength + ' ' + pathLength;
+
+path.style.strokeDashoffset = pathLength;
+
+
+path.getBoundingClientRect();
+
+var tempoCostruzione = 10; // 7 secondi
+var passaggi = 2000; // Numero di passaggi
+
+var intervallo = tempoCostruzione * 100 / passaggi;
+
+var passaggiCompletati = 0;
+
+function eseguiPasso() {
+  var progresso = passaggiCompletati / passaggi;
+
+  var drawLength = pathLength * progresso;
+
+  path.style.strokeDashoffset = pathLength - drawLength;
+
+  if (passaggiCompletati >= passaggi) {
+    clearInterval(intervalloCostruzione);
+    path.style.strokeDasharray = "none";
+    path.style.fill = "#FDFDFD";
+  } else {
+    passaggiCompletati++;
+  }
+}
+
+var intervalloCostruzione = setInterval(eseguiPasso, intervallo);
