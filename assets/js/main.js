4a71d9a400b617b657d2aa8d5af7d231306b3a47
Lorenzo <92269232+Lorenzoptg@users.noreply.github.com>
2023-11-17 10:36:08 +0100

    aggiornamenti

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,30 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+var path = document.querySelector('#star-path');
+      var pathLength = path.getTotalLength();
+      path.style.strokeDasharray = pathLength + ' ' + pathLength;
+      path.style.strokeDashoffset = pathLength;
+      path.getBoundingClientRect();
+      var tempoCostruzione = 7; // 7 secondi
+      var passaggi = 100; // Numero di passaggi
+      var intervallo = tempoCostruzione * 500 / passaggi;
+      var passaggiCompletati = 0;
+      function eseguiPasso() {
+      var progresso = passaggiCompletati / passaggi;
+      var drawLength = pathLength * progresso;
+      path.style.strokeDashoffset = pathLength - drawLength;
+      if (passaggiCompletati >= passaggi) {
+          clearInterval(intervalloCostruzione);
+          path.style.strokeDasharray = "none";
+          path.style.fill = "#FDFDFD";
+      } else {
+          passaggiCompletati++;
+      }
+      }
+      var intervalloCostruzione = setInterval(eseguiPasso, intervallo);
+
 (function() {
   "use strict";
 
